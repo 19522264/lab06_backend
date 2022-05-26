67c29a41899d448c971666ae002e2a5c30fd8565
19522264 <19522264@gm.uit.edu.vn>
2022-05-27 02:37:36 +0700

    0237

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("getquestion")
+  async getQuestion() {
+    return await this.appService.getQuestion();
+  }
+  @Post("submit")
+  async postSubmit(
+    @Body("data") data : string,
+    
+  ){
+     return await this.appService.postSubmit(data) 
+  }
+  
+  @Get("leaderBoard")
+  async getleaderBoard() {
+    return await this.appService.getleaderBoard();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,129 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prismaService: PrismaService){}
+  async getQuestion() {
+    const randomlessthan2 = Math.floor(Math.random() * 2) // for prince and princess
+    const randomlessthan3 = Math.floor(Math.random() * 3)
+    //console.log(randomlessthan2, randomlessthan3)
+    const knight =  await this.prismaService.knight.findFirst({
+      skip: randomlessthan3
+    })
+    const calvinothecook = await this.prismaService.calvinothecook.findFirst({
+      skip: randomlessthan3
+    })
+    const trustedservant = await this.prismaService.trustedservant.findFirst({
+      skip: randomlessthan3
+    })
+    const prince = await this.prismaService.prince.findFirst({
+      skip: randomlessthan2
+    })
+    const princesszizola = await this.prismaService.princesszizola.findFirst({
+      skip: randomlessthan2
+    })
+    const alchemist = await this.prismaService.alchemist.findFirst({
+      skip: randomlessthan3
+    })
+    const queen = await this.prismaService.queen.findFirst({
+      skip: randomlessthan3
+    }) 
+    const prisoner = await this.prismaService.prisoner.findFirst({
+      skip: randomlessthan3
+    })
+    const merchant = await this.prismaService.merchant.findFirst({
+      skip: randomlessthan3
+    })
+    const farmer = await this.prismaService.farmer.findFirst({
+      skip: randomlessthan3
+    })
+    const questions = {
+      qMaxPoint: 8719,
+      qLength: 10,
+      characters: []
+    }
+    questions.characters.push({
+      ...knight,
+      contentUrl: 'knight.png',
+      name: "Knight"
+    })
+    questions.characters.push({
+      ...calvinothecook,
+      contentUrl: 'the-cook.png',
+      name: "Calvino the cook"
+    })
+    questions.characters.push({
+      ...trustedservant,
+      contentUrl:  "servant.png",
+      name : "Trusted servant"
+    })
+    questions.characters.push({
+      ...prince,
+      contentUrl:  "prince.png",
+      name : "Prince"
+    })
+    questions.characters.push({
+      ...princesszizola,
+      contentUrl:  "princess.png",
+      name : "Prince"
+    })
+    questions.characters.push({
+      ...alchemist,
+      contentUrl:  "achemist.png",
+      name : "Prince"
+    })
+    questions.characters.push({
+      ...queen, 
+      contentUrl: "queen.png",
+      name: "Queen"
+    })
+    questions.characters.push({
+      ...prisoner,
+      contentUrl: "prisoner.png",
+      name: "Prisoner"
+    })
+    questions.characters.push({
+      ...merchant,
+      contentUrl: "merchant.png",
+      name: "Merchant"
+    })
+    questions.characters.push({
+      ...farmer,
+      contentUrl: "farmer.png",
+      name: "Farmer"
+    })
+    return questions
+  }
+  async postSubmit(data : string){
+    const parsed = JSON.parse(data)
+    const result = await this.prismaService.user.create({
+      data:{
+        name: parsed.user,
+        score: parsed.score
+      }
+    })
+    if (result){
+      return "ok"
+    }
+    return "fail"
+  }
+  async getleaderBoard(){
+    const result = await this.prismaService.user.findMany({
+      orderBy: {
+        score: 'desc'
+      }
+    })
+    let leaders = []
+    let rank = 1;
+    for(const index of result){
+      
+      leaders.push({
+        ...index,
+        rank
+      })
+      rank += 1
+    }
+    return leaders
   }
 }
